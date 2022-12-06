14f59cdbb47e35878a3d47dbd642a3ef3e6380d8
MariaTrue <97883608+MariaTrue@users.noreply.github.com>
2022-12-06 20:45:57 +0200

    create server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`listening port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(express.static("public"));
