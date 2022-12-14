53bfd0ca1fd083536f8fd42e83eb85fb01fbb6aa
MariaTrue <97883608+MariaTrue@users.noreply.github.com>
2022-12-14 20:58:20 +0200

    data base file on server

---
+++ dbServer.js
@@ -0,0 +1,24 @@
+const db = {
+    todos: [],
+    filter: "ALL_TASKS",
+    getTodos: () => this.todos,
+    addTodo(name) {
+        this.todos.push({
+            name,
+            completed: false,
+            id: Math.ceil(Math.random() * 10000),
+        })
+    },
+    deleteTodo(id) {
+        this.todos = this.todos.filter((todo) => todo.id !== id)
+    },
+    toggleTodo(id) {
+        this.todos.forEach((todo) => {
+            if(todo.id == id) {
+                todo.completed = !todo.completed;
+            }
+        })
+    },
+    getFilter: () => this.filter,
+    setFilter: (filter) => this.filter = filter
+}
